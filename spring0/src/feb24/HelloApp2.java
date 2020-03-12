8782104684148f6fa9c678d7241c68a6a2152a71
eunsolhong <hongsol.dev@gmail.com>
2020-03-12 18:31:12 +0900

    20200312

---
+++ spring0/src/feb24/HelloApp2.java
@@ -0,0 +1,25 @@
+package feb24;
+
+public class HelloApp2 {
+	public static void main(String[] args) {
+		MessageBean bean = new MessageBeanKr();
+		bean.sayHello("Spring");
+	}
+
+}
+
+interface MessageBean{
+	public void sayHello(String name);
+}
+
+class MessageBeanEn implements MessageBean{
+	public void sayHello(String name) {
+		System.out.println("Hello, " + name + "!");
+	}
+}
+
+class MessageBeanKr implements MessageBean {
+	public void sayHello(String name) {
+		System.out.println("안녕하세요, " + name + " 님." );
+	}
+}
---
+++ spring0/src/feb24/HelloApp3.java
@@ -0,0 +1,15 @@
+package feb24;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+public class HelloApp3 {
+	public static void main(String[] args) {
+		
+		BeanFactory factory = new XmlBeanFactory(
+				new FileSystemResource("src/xml/applicationContext.xml"));
+		MessageBean bean = factory.getBean("messageBean", MessageBean.class);
+		bean.sayHello("Spring");
+	}
+}
